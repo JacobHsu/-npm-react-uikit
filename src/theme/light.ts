c97afcb006911072f6cc62b6f3d53b380ea0e1d2
jacobhsu <jacob.hsu.tw@gmail.com>
2022-01-26 15:04:40 +0800

    fix: SearchModal theme.modal.background

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,6 +1,7 @@
 import { DefaultTheme } from "styled-components";
 import { light as lightCard } from "../components/Card/theme";
 import { light as lightNav } from "../widgets/Menu/theme";
+import { light as lightModal } from "../widgets/Modal/theme";
 import base from "./base";
 import { lightColors } from "./colors";
 
@@ -10,6 +11,7 @@ const lightTheme: DefaultTheme = {
   colors: lightColors,
   card: lightCard,
   nav: lightNav,
+  modal: lightModal,
 };
 
 export default lightTheme;
